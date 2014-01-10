4f140fc9137cfd417550bba641b4b1013188f8e1
jbudynk <jbudynkiewicz@cfa.harvard.edu>
2014-01-10 17:48:47 -0500

    Add tests for normalization with correct_flux=True

--- tests/testAggregateSed.py
+++ tests/testAggregateSed.py
@@ -2,6 +2,7 @@ import numpy
 import unittest
 import warnings
 import logging
+import matplotlib.pyplot as plt
 from sedstacker.sed import AggregateSed, Sed, Spectrum, Segment
 from sedstacker.exceptions import *
 
@@ -137,6 +138,45 @@ class TestAggregateSed(unittest.TestCase):
         self.assertAlmostEqual(norm_aggsed.segments[0].norm_constant, 2.0288029e-08)
 
 
+    def test_norm_by_int_correct_flux(self):
+        
+        segment1 = Spectrum(x=numpy.arange(1000,10000,10),
+                            y=numpy.arange(1000,10000,10),
+                            yerr=numpy.arange(1000,10000,10)*.01,
+                            z=0.0)
+        segment2 = Sed(x=numpy.arange(1000,10000,500),
+                       y=numpy.arange(1000,10000,500),
+                       yerr=numpy.arange(1000,10000,500)*.01,
+                       z=0.0)
+        segment3 = Sed(x=numpy.arange(1000,10000,500),
+                       y=numpy.arange(1000,10000,500),
+                       yerr=numpy.arange(1000,10000,500)*.01,
+                       z = 0)
+        segment4 = Spectrum(x=numpy.arange(1000,10000,10),
+                            y=numpy.arange(1000,10000,10),
+                            yerr=numpy.arange(1000,10000,10)*.01,
+                            z=0)
+
+        aggsed = AggregateSed([segment1, segment2, segment3, segment4])
+
+        norm_aggsed = aggsed.normalize_by_int(correct_flux=True, z0=[1,.5,.1,1])
+
+        aggsed[0].z=1.0
+        aggsed[1].z=0.5
+        aggsed[2].z=0.1
+        aggsed[3].z=1.0
+        rf_aggsed=aggsed.shift(0.0)
+
+        control_norm_aggsed = rf_aggsed.normalize_by_int()
+
+        self.assertAlmostEqual(norm_aggsed.segments[0].norm_constant, control_norm_aggsed[0].norm_constant)
+        numpy.testing.assert_array_almost_equal(control_norm_aggsed[0].y,norm_aggsed[0].y)
+        sed=norm_aggsed[1].toarray()
+        control_sed=control_norm_aggsed[1].toarray()
+        numpy.testing.assert_array_almost_equal(control_sed[1],sed[1])
+        numpy.testing.assert_array_almost_equal(control_sed[0],sed[0])
+
+
     def test_norm_at_point(self):
 
         segment1 = Spectrum(x = numpy.arange(1000,10000,10),
@@ -165,6 +205,46 @@ class TestAggregateSed(unittest.TestCase):
         sedarray = norm_aggsed[2].toarray()
         numpy.testing.assert_array_almost_equal(sedarray[1], control_norm_aggsed_segment3)
         self.assertEqual(norm_aggsed[0].norm_constant, 0.2)
+
+
+    def test_norm_at_point_correct_flux(self):
+
+        segment1 = Spectrum(x = numpy.arange(1000,10000,10),
+                            y = numpy.arange(1000,10000,10),
+                            yerr=numpy.arange(1000,10000,10)*.01,
+                            z=0)
+        segment2 = Sed(x=numpy.arange(1000,10000,500),
+                       y=numpy.arange(1000,10000,500),
+                       yerr=numpy.arange(1000,10000,500)*.01,
+                       z=0.0)
+        segment3 = Sed(x=numpy.arange(1000,10000,500),
+                       y=numpy.arange(1000,10000,500),
+                       yerr=numpy.arange(1000,10000,500)*.01,
+                       z = 0)
+        segment4 = Spectrum(x = numpy.arange(1000,10000,10),
+                            y = numpy.arange(1000,10000,10),
+                            yerr=numpy.arange(1000,10000,10)*.01,
+                            z=0)
+
+        aggsed = AggregateSed([segment1, segment2, segment3, segment4])
+        norm_aggsed = aggsed.normalize_at_point(5000,1000,
+                                                correct_flux=True,
+                                                z0=[1.0,0.5,0.35,1.0])
+
+        aggsed[0].z=1.0
+        aggsed[1].z=0.5
+        aggsed[2].z=0.35
+        aggsed[3].z=1.0
+        rf_aggsed=aggsed.shift(0.0)
+
+        control_norm_aggsed = rf_aggsed.normalize_at_point(5000,1000)
+        control_sed=control_norm_aggsed[1].toarray()
+
+        self.assertAlmostEqual(norm_aggsed.segments[2].norm_constant, control_norm_aggsed[2].norm_constant)
+        numpy.testing.assert_array_almost_equal(control_norm_aggsed[0].y,norm_aggsed[0].y)
+        sed=norm_aggsed[1].toarray()
+        control_sed=control_norm_aggsed[1].toarray()
+        numpy.testing.assert_array_almost_equal(control_sed[0],sed[0])
 
 
     def test_remove_segment(self):
@@ -223,6 +303,13 @@ class TestAggregateSed(unittest.TestCase):
         self.assertEqual(len(aggsed.x[4]), len(segment3))
         
 
+#        sed2=norm_aggsed[1].toarray()
+#        sed3=norm_aggsed[2].toarray()
+#        plt.loglog(norm_aggsed[0].x,norm_aggsed[0].x,'r')
+#        plt.loglog(sed2[0],sed2[1],'ko')
+#        plt.loglog(sed3[0],sed3[1],'go')
+#        plt.loglog(norm_aggsed[3].x,norm_aggsed[3].x,'b')
+#        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
