56e50b9ccfe31afdb8aa55f1be66e552772f3e9f
jbudynk <jbudynkiewicz@cfa.harvard.edu>
2014-01-14 13:37:11 -0500

    Adds NUMERIC_TYPE tuple variable

--- sedstacker/config.py
+++ sedstacker/config.py
@@ -1,6 +1,8 @@
 #
-
+from numpy import float_, int_
+from types import FloatType, IntType
 # list of acceptable null values
 # when reading data from file
 NONE_VALS = (None, 'None', 'none', 'null', 'nan', 'NaN', -99., -99)
+NUMERIC_TYPES = (FloatType, IntType, float_, int_)
 
